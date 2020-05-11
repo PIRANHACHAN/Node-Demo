6db605a04375d8f68377b3e26f9ea4ba67415b43
PIRANHA <kobeel@163.com>
2020-05-11 15:19:47 +0800

    update

--- feedback/app.js
+++ feedback/app.js
@@ -3,10 +3,20 @@ const fs = require('fs')
 
 http
   .createServer(function (req, res) {
-    if (req.url === '/') {
+    let url = req.url
+    if (url === '/') {
       fs.readFile('./views/index.html', function (err, data) {
         if (err) {
           res.end('404 No Found')
+          return
+        }
+        res.end(data)
+      })
+    }
+    if (url.indexOf('/public/') === 0) {
+      fs.readFile('.' + url, function (err, data) {
+        if (err) {
+          res.end('404 Not Found')
           return
         }
         res.end(data)
